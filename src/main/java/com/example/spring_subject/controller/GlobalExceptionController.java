3750690e3556469368f3b6c1d0e02e44e0ee8f3c
Rio-323 <kimsiyeon5214@gmail.com>
2022-10-18 23:07:16 +0900

    @RestControllerAdvice를 활용한 예외처리

---
+++ src/main/java/com/example/spring_subject/controller/GlobalExceptionController.java
@@ -0,0 +1,15 @@
+package com.example.spring_subject.controller;
+
+import com.example.spring_subject.global.dto.GlobalRequestDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionController {
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public GlobalRequestDto customExceptionHandler(IllegalArgumentException ex) {
+        return new GlobalRequestDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value () );
+    }
+}
